cef8f49a23bb33cf53bd421e2a382e6afd01c1aa
Anders Holst <andersthereseholst@gmail.com>
2024-09-21 08:41:56 +0000

    update post

--- coder-community-frontend/src/pages/PostPage.js
+++ coder-community-frontend/src/pages/PostPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
+import axiosInstance from '../axiosInstance';
 
 const PostPage = () => {
   const [posts, setPosts] = useState([]);
@@ -8,7 +8,7 @@ const PostPage = () => {
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const response = await axios.get('/api/posts/');
+        const response = await axiosInstance.get('/posts/');
         setPosts(response.data);
       } catch (error) {
         console.error('Failed to fetch posts', error);
